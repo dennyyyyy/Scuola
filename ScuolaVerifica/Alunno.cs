e82c565f8e5ed82d45abfd4dd5869b0b810cfe9b
Dennis Comandini <DENNIS.COMANDINI@local.ittcesena.it>
2023-03-16 08:42:52 +0100

    alunno finito

--- ScuolaVerifica/Alunno.cs
+++ ScuolaVerifica/Alunno.cs
@@ -8,5 +8,33 @@ namespace ScuolaVerifica
 {
     internal class Alunno
     {
+        public  string Nome  { get; set; }
+        public  string cognome { get; set; }
+        public  int eta { get; set; }
+        public  string citta { get; set; }
+        
+        public Alunno(string s, string c, int e , string cit )
+        {
+            Nome = s;
+            cognome = c;
+            eta = e;
+            citta = cit;
+        }
+         public override string  ToString()
+        {
+
+            return Nome + " " + cognome+" " + eta +" "+ citta;
+        }
+        public string Verificabiennio(Alunno n)
+        {
+            if (n.eta < 16)
+            {
+                return "biennio";
+            }
+            else
+            {
+                return "triennio";
+            }
+        }
     }
 }
