069b15ec63c5bcef45706956b5830794a239d59f
Dennis Comandini <DENNIS.COMANDINI@local.ittcesena.it>
2023-03-16 09:06:33 +0100

    scuola finita

--- ScuolaVerifica/Scuola.cs
+++ ScuolaVerifica/Scuola.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,30 @@ namespace ScuolaVerifica
         public  string Nome { get; set; }
         public  string indirizzo { get; set; }
         public string orari   { get; set; }
+        public  List<Alunni>  Lista { get; set; }
+
+        public Scuola(string nome, string indirizzo, string orari, List<Alunni> lista)
+        {
+            Nome = nome;
+            this.indirizzo = indirizzo;
+            this.orari = orari;
+            Lista =lista;
+        }
+        public void aggiunta(Alunno n )
+        {
+            Lista.Add(n);
+        }
+        public List<Alunno> Ricerca(string c)
+        {
+            List<Alunno> lc =new List<Alunno>(); 
+            for(int i=0; i<Lista.Count; i++)
+            {
+                if (Lista[i].Cognome== c)
+                {
+                    lc[i].Add(Lista[i].Cognome);
+                }
+            }
+            return lc;
+        }
     }
 }
