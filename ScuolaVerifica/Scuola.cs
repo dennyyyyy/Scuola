597527bda973af1ed38c00c5e5656c72498ea7ac
Dennis Comandini <DENNIS.COMANDINI@local.ittcesena.it>
2023-03-16 08:50:18 +0100

    Scuola inizio

---
+++ ScuolaVerifica/Scuola.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScuolaVerifica
+{
+    internal class Scuola
+    {
+        public  string Nome { get; set; }
+        public  string indirizzo { get; set; }
+        public string orari   { get; set; }
+    }
+}
